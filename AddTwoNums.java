e177f8d6f7bc37b8ee3339bf217cfde1b966d779
Alondra Rodriguez <arodriguez10@daltonstate.edu>
2023-08-17 17:33:28 -0700

    my first commit

---
+++ AddTwoNums.java
@@ -0,0 +1,9 @@
+public class AddTwoNums {
+   public static void main(String[] args) {
+      int parisAge = 10;
+      int doubleParisAge = parisAge * 2;
+      System.out.println(doubleParisAge);
+      System.out.println(parisAge);
+
+   }
+}
